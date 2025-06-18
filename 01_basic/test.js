fea21a1aa229095fc514af59d8223eabf66b6f35
Altamash Naseem <126944445+altamashnaseem@users.noreply.github.com>
2025-06-18 13:20:55 +0000

    variables

---
+++ 01_basic/test.js
@@ -0,0 +1,16 @@
+//console.log("hello jii")
+// wil print hello jii 
+// variable 
+
+const empollyId = 1234533
+let age =34
+var city = "darbhanga" // var has block scope and function scope proble dont use 
+area = "shivdhara"
+// here  empollyId is assing as const it can't be change 
+//empollyId =23
+
+age=4
+city = "Patna" 
+area = "booring"
+
+console.table([empollyId, age, city, area])
