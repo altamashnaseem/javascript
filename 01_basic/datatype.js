be34755ba046cf42a9ae0d441b2ed3f432f96201
Altamash Naseem <126944445+altamashnaseem@users.noreply.github.com>
2025-06-18 19:21:02 +0000

    datatype

---
+++ 01_basic/datatype.js
@@ -0,0 +1,24 @@
+// there are 2 main type of data type primative and non primative 
+//primative datatype
+let age= 14  // number datatype
+let name = "rehan"  // string datatype
+let loggin = true  // boolean datatype
+let id= undefined  // this is undefined data type
+let weather = null   // this is null where nothing is assing object datatype
+let username = Symbol(123) // this symbol datatype is used for unique 
+let binint = 243483767283n // this is big int to store big intergers
+
+// conversion of number into string
+
+let num = String(age)
+console.log(typeof num)
+
+// non primative are  object {}, array[], function()
+
+const myobj={
+    age : 12,
+    name : "altamsh",
+    id : "12"
+}
+console.log(myobj.name)
+
